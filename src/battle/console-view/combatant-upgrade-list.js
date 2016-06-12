8e3bb4d76a6b4dc01fa8f65614a6d7ff28f665d4
Drew Conley <drewconley@gmail.com>
2016-06-12 08:22:57 -0500

    display upgrades and make them removable

--- src/battle/console-view/combatant-upgrade-list.js
+++ src/battle/console-view/combatant-upgrade-list.js
@@ -1,20 +1,57 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import {setCombatantValue} from '../../redux-action-creators/battle-action-creators'
+
 
 @connect((state, props) => {
     return {
+        combatant: state.battle.combatants[props.combatantId] || {}
     }
 })
 
 class CombatantUpgradeList extends React.Component {
 
+    handleRemove(index) {
+        setCombatantValue(this.props.combatantId, {
+            upgrades: this.props.combatant.upgrades.filter((u,i) => {
+                return i != index
+            })
+        })
+    }
+
     render() {
+        const upgradeList = this.props.combatant.upgrades.map((u,i) => {
+            return (
+                <CombatantUpgrade key={i} handleRemove={::this.handleRemove} listIndex={i} libraryId={u.libraryId} />
+            )
+        });
         return (
            <div>
-               List of Upgrades ({this.props.combatantId})
+               <div>Upgrades</div>
+               <div>
+                   {upgradeList}
+               </div>
            </div>
         );
     }
 }
 
 export default CombatantUpgradeList;
+
+import LaptopUpgrades from '../../_data/_laptop-upgrades'
+class CombatantUpgrade extends React.Component {
+
+    handleRemove() {
+        this.props.handleRemove(this.props.listIndex);
+    }
+
+    render() {
+        const model = LaptopUpgrades[this.props.libraryId];
+        return (
+            <div>
+                <span>{model.name}</span>
+                <button onClick={::this.handleRemove}>Remove</button>
+            </div>
+        )
+    }
+}
--- src/battle/get-class-stats-by-level.js
+++ src/battle/get-class-stats-by-level.js
@@ -1,9 +1,7 @@
 import Classes from '../_data/_classes'
 
 export function getClassStatsByLevel(characterClass="", level=1) {
-
-    console.log(characterClass)
-
+    
     const classModel = Classes[characterClass];
 
     const arrayIndex = level - 1; /* Stat array[0] = level 1 */
