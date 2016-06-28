0f4a8e9d1098fcb8c9019ca20b103ff163b39b35
Drew Conley <drewconley@gmail.com>
2016-06-27 21:03:27 -0500

    add todo list

--- src/_data/battle-actions.js
+++ src/_data/battle-actions.js
@@ -7,3 +7,44 @@ export default {
     //...itemActions,
     //...naturals
 }
+/*
+ TODO: TODO: Insult
+ // TODO: Slice
+ // TODO: Slice Mk II
+ // TODO: Slice Mk III
+ // TODO: Preloaded
+ // TODO: Preloaded Mk II
+ // TODO: Preloaded Mk III
+ TODO: forEach
+ TODO: forEach Mk II
+ TODO: for Each Mk III
+ TODO: map
+ TODO: map Mk II
+ TODO: map Mk III
+ TODO: reduce
+ TODO: reduce Mk II
+ TODO: reduce Mk III
+ TODO: Curl
+ TODO: Curl Mk II
+ TODO: Curl Mk III
+ TODO: Steal-and-Use (needs a name)
+ TODO: Promise
+ TODO: -Promise resolve
+ TODO: Promise Mk II
+ TODO: Promise Mk III
+ TODO: Garbage Jammer
+ TODO: Thrash
+ TODO: -Thrash: Catch on Fire
+ TODO: Repeat
+ TODO: Revert
+ TODO: Zen
+ TODO: -Come out of Zen
+ TODO: Fury
+ TODO: -Come out of Fury
+ TODO: Throttle
+ TODO: DDoS
+ TODO: Rsync
+ TODO: Deadline
+ TODO: Redirect
+ TODO: Clean
+ */
