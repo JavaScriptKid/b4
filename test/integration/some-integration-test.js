92bdf9882d2591bbdc9c16b64dd6a94e7ccb19da
dc901242 <drewconley@gmail.com>
2016-06-30 10:43:49 -0500

    first integration file

---
+++ test/integration/some-integration-test.js
@@ -0,0 +1,11 @@
+import {assert} from 'chai'
+import store from '../../src/init/store'
+import initBattleCombatants from '../../src/battle/init-battle-combatants'
+
+describe('Integration', () => {
+    it('can can go from submissions to rollout queue', () => {
+        initBattleCombatants();
+        const state = store.getState().battle;
+        console.log(state)
+    });
+});
