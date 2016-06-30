b97703243a702a98cd078135dd49928458094b03
dc901242 <drewconley@gmail.com>
2016-06-30 13:03:22 -0500

    add submission mock to integration test

--- test/integration/some-integration-test.js
+++ test/integration/some-integration-test.js
@@ -5,9 +5,24 @@ import initBattleCombatants from '../../src/battle/init-battle-combatants'
 describe('Integration', () => {
     it('can can go from submissions to rollout queue', () => {
         initBattleCombatants();
-        const state = store.getState().battle;
+        const history = store.getState().battle.history;
+        const timeIndex = store.getState().battle.devTimeTravelTurn;
+        const combatants = { ...history[timeIndex].combatants};
 
+        const submissions = [
+            {
+                casterId: Object.keys(combatants)[0],
+                targetId: Object.keys(combatants)[1],
+                actionId: "attack-001-a"
+            },
+            {
+                casterId: Object.keys(combatants)[1],
+                targetId: Object.keys(combatants)[0],
+                actionId: "attack-002-a"
+            }
+        ];
 
-        console.log(state)
+        console.log(combatants)
+        console.log(submissions)
     });
 });
