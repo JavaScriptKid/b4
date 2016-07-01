2ac0a2a739a4aa1ec6b2c055b3b29f2f34aae663
dc901242 <drewconley@gmail.com>
2016-07-01 16:46:32 -0500

    add documentation

--- src/battle/execute-turn.js
+++ src/battle/execute-turn.js
@@ -9,16 +9,20 @@ import {setBattleValue} from '../redux-action-creators/battle-action-creators'
  */
 export function executeTurn(submissionModels=[]) {
 
+    /* 1. Assemble the Queue */
+    //------------------------
     const lastHistoryEntry = store.getState().battle.history[store.getState().battle.history.length-1];
-
     const actionQueue = [
         ...extractQueuedSubmissions(lastHistoryEntry.cloudQueue), //Need a test that proves this adds these
         ...getOrderedActionsFromSubmissions(submissionModels)
     ];
-
-    console.log(actionQueue)
-
-    setBattleValue({
+    
+    setBattleValue({ /* Remove this turn from the queue */
         queueCloud: removeQueueSlot( store.getState().battle.queueCloud )
     });
+
+    console.log(actionQueue);
+
+    /* 2. processActions() ? */
+    //------------------------
 }
