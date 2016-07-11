7d01e03dbb38b6002838adea6d7771d06e7eddf1
dc901242 <drewconley@gmail.com>
2016-07-11 15:27:44 -0500

    initial steps for attacks

--- src/battle/arena-view/submission-ui/get-submission-menu-structure.js
+++ src/battle/arena-view/submission-ui/get-submission-menu-structure.js
@@ -1,6 +1,9 @@
+import Actions from '../../../_data/battle-actions'
+
 const optionSchema = {
     labelText: "some option",
     customClasses: "",
+    descriptionBarText: "",
     filterPresenceTest: function() {
         return true
     },
@@ -13,11 +16,35 @@ const optionSchema = {
 var filterOptionsByTest = function(optionsList) {
     return optionsList.filter(optionModel => {
         return optionModel.filterPresenceTest()
-    })
+    }).map(optionModel => {
+        return {
+            ...optionModel,
+            isDisabled: optionModel.filterDisabledTest()
+        }
+    });
 };
 
-export function getSubmissionMenuStructure(casterModel, menuLevel="", menuPage="") {
+export function getSubmissionMenuStructure(casterModel, menuLevel="", menuStartingIndex=0) {
 
+
+    /* Normal attacks */
+    if (menuLevel == "attacks") {
+        const attackOptions = casterModel.attacks.filter(atkId => {
+            const model = Actions[atkId];
+            return model.type == "Normal"
+        }).map(atkId => {
+            const model = Actions[atkId];
+            return {
+                ...optionSchema,
+                labelText: model.name,
+            };
+        });
+
+        return filterOptionsByTest([
+            ...attackOptions,
+            ...
+        ]);
+    }
 
     /* Default - root level menu */
     return filterOptionsByTest([
