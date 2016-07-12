4e4786341bb38d4e6b0c980fc707851305b3b375
Drew Conley <drewconley@gmail.com>
2016-07-11 23:42:27 -0500

    add PP and change arrow buttons

--- src/battle/arena-view/submission-ui/submission-menu.js
+++ src/battle/arena-view/submission-ui/submission-menu.js
@@ -19,23 +19,27 @@ class SubmissionMenu extends React.Component {
         const backStyle = {
             ...baseOptionStyles,
             width: 'initial',
-            flex: "1",
+            //flex: "1",
             marginTop: 0,
             textAlign: "center",
+            display:"block"
         };
         
         const containerStyle = {
             width: baseUnit * 37,
+            display: "flex",
+            justifyContent: "space-between"
         };
         const pageStyle = {
             ...baseOptionStyles,
             width: baseUnit * 8,
             marginLeft: baseUnit * 1.5,
-            marginTop: 0
+            marginTop: 0,
+
         };
 
         const backModel = {
-            labelText: "MENU",
+            labelText: "BACK",
             customClasses: "back-button",
         };
 
@@ -50,13 +54,17 @@ class SubmissionMenu extends React.Component {
             supportText: null
         };
 
-        
+
 
         return (
             <div className="submission-menu_more-container" style={containerStyle}>
-                    <SubmissionMenuOption vW={this.props.vW} useBackArrowIcon={true} baseStyle={backStyle} model={backModel} />
+                <div>
+                    <SubmissionMenuOption vW={this.props.vW} useBackArrowIcon={false} baseStyle={backStyle} model={backModel} />
+                </div>
+                <div style={{display:"flex"}}>
                     <SubmissionMenuOption vW={this.props.vW} isLeftArrow={true} baseStyle={pageStyle} model={pageUpModel} />
                     <SubmissionMenuOption vW={this.props.vW} isRightArrow={true} baseStyle={pageStyle} model={pageDownModel} />
+                </div>
             </div>
         )
     }
@@ -69,11 +77,23 @@ class SubmissionMenu extends React.Component {
             fontSize: baseUnit * 3
         };
         const optionStyle = {
-            padding: baseUnit * 1.5,
+            padding: baseUnit * 1.2,
             width: baseUnit * 37,
             border: `${baseUnit * 0.5 }px solid #000`,
             borderRadius: `${baseUnit * 0.5 }`,
             marginTop: baseUnit * 1.5
+        };
+        const ppStyle = {
+            float: "right",
+            fontSize: baseUnit * 2,
+            //marginBottom: baseUnit * 1,
+            borderRadius: baseUnit * 0.3,
+            position: "relative",
+            borderBottomRightRadius: 0,
+            top: 2,
+            background: "#000",
+            color: "#fff",
+            padding: `${baseUnit * 0.5}px ${baseUnit}px`
         };
 
 
@@ -89,7 +109,10 @@ class SubmissionMenu extends React.Component {
 
         return (
            <div style={menuStyle} className="submission-menu">
-               {optionComponents}
+               <div style={ppStyle}>PP {this.props.casterModel.pp}/{this.props.casterModel.maxPp}</div>
+               <div>
+                {optionComponents}
+               </div>
 
                <PagingIndicators
                    vW={this.props.vW}
