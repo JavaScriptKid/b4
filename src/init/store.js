3e0681c21007f1ebdf1cc9f837a8bc58561d0563
Drew Conley <drewconley@gmail.com>
2016-07-31 20:04:31 -0500

    update store for latest demo

--- src/init/store.js
+++ src/init/store.js
@@ -35,7 +35,7 @@ const store = createStore({
         menuLevel: "", //"", "attacks", "special", "items", "charged-attacks", "charged-frameworks"
         menuOptionIndex: 1,
         selectedOptionId: "",
-        viewMode: "console", //"console" || "battle" || "reporting"
+        viewMode: "battle", //"console" || "battle" || "reporting"
 
         //Arena
         backgroundStyle: null
