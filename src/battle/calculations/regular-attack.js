907695b8a77100c6a169f40102ebeb2a47014961
dc901242 <drewconley@gmail.com>
2016-08-09 17:22:11 -0500

    add the new iterator values

--- src/battle/calculations/regular-attack.js
+++ src/battle/calculations/regular-attack.js
@@ -13,8 +13,13 @@ export function getRegularAttackChanges(action, casterModel, targetModel, curren
             //Do the roll [x,x] amount of times
             const repetitionsCount = getRandomInRange( action.repetitions[0], action.repetitions[1]);
 
-            const damageRange = [2,3,4,5,6];
+            const damageRange = Array.isArray(action.affectTargetHpPoints)
+                ? action.affectTargetHpPoints
+                : [2,3,4,5,6];
+
             const chosenBase = randomFromArray(damageRange);
+
+
 
             let totalDamageCounter = 0;
             range(0, repetitionsCount).forEach((number, index) => {
@@ -42,8 +47,6 @@ export function getRegularAttackChanges(action, casterModel, targetModel, curren
 
             // Point based strike
             if (action.affectTargetHpPoints != 0) {
-                /* TODO: This function still needs to be balanced according to actual character Stats. Right now, everything is 0 */
-
 
                 let attackRoll = casterModel.attackRoll(
                     action.affectTargetHpPoints,
