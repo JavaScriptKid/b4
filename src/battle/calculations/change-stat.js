a98f675e3e16c7e27539b9c6d7eb42b928812d0e
Drew Conley <drewconley@gmail.com>
2016-10-19 15:38:19 -0500

    change really bad NaN bug

--- src/battle/calculations/change-stat.js
+++ src/battle/calculations/change-stat.js
@@ -3,13 +3,13 @@ export function getStatChanges(action, casterModel, targetModel, currentChanges)
     let changes = {};
 
     //Increase/Decrease a Stat with an Item
-    if (action.affectCasterAttackModifier !== 0) {
+    if (typeof action.affectCasterAttackModifier === "number" && action.affectCasterAttackModifier !== 0) {
         changes["affectCasterAttackModifier"] = action.affectCasterAttackModifier
     }
-    if (action.affectCasterDefenseModifier !== 0) {
+    if (typeof action.affectCasterDefenseModifier === "number" && action.affectCasterDefenseModifier !== 0) {
         changes["affectCasterDefenseModifier"] = action.affectCasterDefenseModifier
     }
-    if (action.affectCasterSpeedModifier !== 0) {
+    if (typeof action.affectCasterSpeedModifier === "number" && action.affectCasterSpeedModifier !== 0) {
         changes["affectCasterSpeedModifier"] = action.affectCasterSpeedModifier
     }
 
